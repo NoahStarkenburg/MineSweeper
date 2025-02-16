ecbac08e04d626bef5c4cb8f54f3053b4f5ebf5e
noahs <noahstarkenburg@gmail.com>
2025-02-15 19:59:59 -0700

    added assets as well sliders for start game page

--- MineSweeper/Controllers/GameController.cs
+++ MineSweeper/Controllers/GameController.cs
@@ -6,10 +6,17 @@ namespace MineSweeper.Controllers
     public class GameController : Controller
     {
         [SessionCheckFilter]
+        public IActionResult StartGame(int rows, int columns)
+        {
+            return View();
+        }
+
+        [SessionCheckFilter]
         public IActionResult StartGame()
         {
             return View();
         }
+
         public IActionResult Index()
         {
             return View();
